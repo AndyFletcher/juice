d30964a30034ee2938d20e6a7063342ac5e0d196
= <fletch@yazobo.com>
2009-11-05 13:07:27 +0000

    Changed previous config function to rawConfig which will take any location you want to throw at it. ie you are responsible for providing docRoot or whatever you want.
    Added a new version of config which is "smart" and will load based on docRoot/conf/SOMETHING.json
    
    Application tests updated.

--- tests/application.t.js
+++ tests/application.t.js
@@ -8,12 +8,20 @@ asserts.throws = require( 'tests/helpers' ).throws;
 
 if (!this.exports) this.exports = {};
 
+exports.test_RawConfig = function() {
+  var app = new Application;
+  app.setup( DOC_ROOT );
+  asserts.same( app.rawConfig( "tests/test_conf/test_conf.json" ).test, "ok", "Test config should return 'ok'" );
+  asserts.same( app.rawConfig( "tests/test_conf/test_conf.json" ).not_there, undefined, "Non-existant config should return undefined" );
+  asserts.throws( function() { app.rawConfig( "tests/test_conf/not_there.json" ) }, "Loading non-existant conf file should fail" );
+}
+
 exports.test_Config = function() {
   var app = new Application;
   app.setup( DOC_ROOT );
-  asserts.same( app.config( "tests/test_conf/test_conf.json" ).test, "ok", "Test config should return 'ok'" );
-  asserts.same( app.config( "tests/test_conf/test_conf.json" ).not_there, undefined, "Non-existant config should return undefined" );
-  asserts.throws( function() { app.config( "tests/test_conf/not_there.json" ) }, "Loading non-existant conf file should fail" );
+  asserts.same( app.config( "test_conf" ).test, "ok", "Test config should return 'ok'" );
+  asserts.same( app.config( "test_conf" ).not_there, undefined, "Non-existant config should return undefined" );
+  asserts.throws( function() { app.config( "not_there" ) }, "Loading non-existant conf file should fail" );
 }
 
 if (require.main == module)
